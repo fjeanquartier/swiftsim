1506833d6ff1937a882b8f775a0dd8e477262da1
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-03-07 10:17:51 +0000

    Added a fixed diffusion option

--- examples/HydroTests/Diffusion_1D/run_set.sh
+++ examples/HydroTests/Diffusion_1D/run_set.sh
@@ -4,6 +4,7 @@ beta=(1.0 0.1 0.01 0.001)
 swift_location="../../swift"
 flags="--hydro --limiter --threads=2"
 parameter="diffusion.yml"
+parameter_fixed="diffusion_fixed_alpha.yml"
 make_ic_script="makeIC.py"
 plot_script="plotSolution.py"
 
@@ -15,8 +16,9 @@ do
     python ../$make_ic_script
 
     ../$swift_location $flags -P "SPH:diffusion_beta:${i}" ../$parameter
-
     python ../$plot_script
+    ../$swift_location $flags -P "SPH:diffusion_beta:${i}" ../$parameter_fixed
+    python ../$plot_script -s diffusion_fixed_alpha -o diffusion_fixed_alpha.png
 
     rm *.hdf5
 
